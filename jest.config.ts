96e872b8cb98d21bb318fc1c39162c4ed2577e1c
gabrielgog <gabrielgog96@gmail.com>
2024-03-18 02:01:20 +0100

    update generated jest.config.ts file

--- jest.config.ts
+++ jest.config.ts
@@ -2,18 +2,15 @@ import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   transformIgnorePatterns: [
     "/node_modules/(?!(string-width)/)"
   ],
-  // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
